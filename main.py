1ffccd042455c7e9dd9551ae8bf7d0df85fd6991
Akshay Raju <akshay.raju@macaulay.cuny.edu>
2023-08-17 13:45:03 -0400

    Added functions + command line arguments to main

--- main.py
+++ main.py
@@ -1,9 +1,81 @@
 from IndividualNetwork import IndividualNetwork
 from Structure import Structure
+from MultiNetwork import MultiNetwork
+import argparse
 
-struct = Structure('/data/araju/ptpsinpdb/FinalRefine_qFitV4_V2/6B8Z/6B8Z_qFit.pdb')
-net = IndividualNetwork(struct)
-net.populateNetwork()
+def setupArguments ():
 
-print(struct.model, struct.name)
-print(net.structure, net.name, net.network)
+    # Creates argument parser object
+    parser = argparse.ArgumentParser(description='Multiconformer model network generator')
+    
+    # Adds individual arguments
+    parser.add_argument(
+        'structureListFile', 
+        help='Text file (.txt) of the pathnames to all of the structures'
+    )
+
+    parser.add_argument(
+        'alignmentFile', 
+        help='Sequence alignment file in fasta (.fa) format'
+    )
+
+    parser.add_argument(
+        '-m', 
+        '--multi', 
+        action='store_true', 
+        help='Flag that if present combines and analyzes the multiple networks inputted together'
+    )
+
+    args = parser.parse_args()
+
+    print(args.structureListFile, args.alignmentFile, args.multi)
+    return args
+
+def readFileList (args):
+    
+    fileList = []
+
+    # Opens file of pathnames
+    inputStructureListFile = open(args.structureListFile, "r")
+
+    # Loops through each line (individual path) and adds it to list of pathnames
+    for line in inputStructureListFile:
+        fileList.append(line.rstrip())
+
+    inputStructureListFile.close()
+    return fileList
+
+def generateNetworks (fileList, args):
+
+    if args.multi == True:
+        # Initializes an empty multi-network object 
+        multi = MultiNetwork(args.alignmentFile)
+
+    # Loops over every pathname in the structure pathname list
+    for structPathName in fileList:
+        
+        # Creates a structure object from the structure in pathname
+        struct = Structure(structPathName)
+
+        # Creates an individual network object from the structure object and then populates the network
+        net = IndividualNetwork(struct)
+        net.populateNetwork()
+        net.visualize()
+
+        if args.multi == True:
+            # Adds the individual network object to the multi-network object
+            multi.Add(net.convertToAdjacency(), struct.getName()[0:4], struct.getFirstResi(), False)
+            print(multi.array)
+
+    if args.multi == True:
+        return multi
+    else:
+        return None
+
+def main ():
+    args = setupArguments()
+    fileList = readFileList(args)
+    multiNetwork = generateNetworks(fileList, args)
+
+if __name__ == "__main__":
+    main()
