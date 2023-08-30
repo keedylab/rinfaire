d068cfe36d4548910f9b0a1dd6e5951eef58ee4a
Akshay Raju <akshay.raju@macaulay.cuny.edu>
2023-08-30 18:12:11 -0400

    Created MainFunctions module
    - Used to house all the important functions for running either main script
    - Combined common functions together since many share similar structures
    - For any differences between the two, code blocks are executed based off of a MultiFlag that specifies if the program running is multi or single model

---
+++ multirin/MainFunctions.py
@@ -0,0 +1,155 @@
+import argparse
+import os
+from Structure import Structure
+from IndividualNetwork import IndividualNetwork
+from MultiNetwork import MultiNetwork
+
+def setupArguments (multiFlag):
+
+    # Creates argument parser object
+    parser = argparse.ArgumentParser(
+        description='Multiconformer model network generator',
+        formatter_class=argparse.RawTextHelpFormatter
+    )
+
+    # Options that are specific to multi-model inputs
+    if multiFlag == True:
+    
+        parser.add_argument(
+            'structureFile', 
+            help='List of input structures file in (.txt) format'
+        )
+
+        parser.add_argument(
+            'alignmentFile', 
+            help='Sequence alignment file in fasta (.fa) format'
+        )
+
+        parser.add_argument(
+            '-l', 
+            '--labels',
+            help='File of metadata labels to classify structures in (.csv) format'
+        )
+
+        parser.add_argument(
+            '-f', 
+            '--fetch', 
+            action='store_true', 
+            help='Fetch metadata from PDB API'
+        )
+
+        parser.add_argument( 
+            '--no_norm_struct', 
+            default=False,
+            action='store_true', 
+            help="Turns off the normalization of each structure's network in relation to the others"
+        )
+    
+    # Options specific to single model inputs
+    else:
+
+        parser.add_argument(
+            'structureFile', 
+            help='Input structure file in (.pdb) format'
+        )
+    
+    # Options that are shared between both programs
+
+    parser.add_argument( 
+        '--no_norm_resi', 
+        default=False,
+        action='store_true', 
+        help="Turns off the normalization of each residue-residue pair in the network by the size of the residue"
+    )
+
+    args = parser.parse_args()
+
+    # Checks of the file extension to ensure that it is the correct type
+    # For multi-model inputs
+    if multiFlag == True:
+ 
+        checkExtension(args.structureFile, '.txt', "Structure file must be in .txt format")
+        checkExtension(args.alignmentFile, '.fa', "Sequence file must be in .fa format")
+
+        # Checks only if the user wants to use inputted labels
+        if args.labels is not None:
+            checkExtension(args.labels, '.csv', "Labels file must be in .csv format")
+
+    # For single model inputs
+    else:
+        checkExtension(args.structureFile, '.pdb', "Structure file must be in .pdb format")
+
+    return args
+
+def checkExtension (file, extension, errorMessage):
+    fileSplit = os.path.splitext(file)
+    fileExt = fileSplit[1]
+
+    if fileExt != extension:
+        raise argparse.ArgumentTypeError(errorMessage)
+    
+def readFile (multiFlag, args):
+    
+    fileList = []
+
+    # Trivial case where you have a single model input
+    if multiFlag == False:
+        fileList.append(args.structureFile)
+    
+    # Case when you have a multi-model input
+    else:
+        # Opens file of pathnames
+        inputStructureListFile = open(args.structureFile, "r")
+
+        # Loops through each line (individual path) and adds it to list of pathnames
+        for line in inputStructureListFile:
+            fileList.append(line.rstrip())
+
+        inputStructureListFile.close()
+    
+    return fileList
+
+def generateIndividualNetworks (fileList, args):
+
+    networkList = []
+
+    # Loops over every pathname in the structure pathname list
+    for structName in fileList:
+        
+        # Creates a structure object from the structure in pathname
+        struct = Structure(structName, args)
+
+        # Creates an individual network object from the structure object and then populates the network
+        net = IndividualNetwork(struct, args)
+        net.populateNetwork()
+
+        # Appends this to the list of networks
+        networkList.append(net)
+
+        # Creates the pyvis visualization (as an .html output)
+        net.visualize()
+
+    return networkList
+
+def generateMultiNetwork (networkList, args):
+    
+    # Initializes an empty multi-network object 
+    multi = MultiNetwork(args)
+
+    # Loops over each network in the list
+    for net in networkList:
+
+        # Adds the individual network object to the multi-network object
+        multi.add(
+            net.convertToAdjacency(), 
+            net.struct.getName()[0:4], 
+            net.struct.getFirstResi()
+        )
+
+    # Calculates the sum matrix of all the structures in the object
+    # Does this across each residue pairing
+    sumMatrix = multi.sum()
+    multi.visualize(sumMatrix, 'sumNetwork')
+
+    return multi
+    
