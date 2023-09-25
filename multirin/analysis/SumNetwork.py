961fdcfc071f11572868a5f280b9fd634282701d
Akshay Raju <akshay.raju@macaulay.cuny.edu>
2023-09-25 18:47:29 -0400

    Created SumNetwork class
    - This will house all functions related to the creation of the SumNetwork

--- multirin/analysis/SumNetwork.py
+++ multirin/analysis/SumNetwork.py
@@ -2,65 +2,78 @@ import xarray as xr
 import networkx as nx
 from pyvis.network import Network
 import logging
+import pickle
 
-def sum (self):
+class SumNetwork:
 
-    # Calculates the sum across the network dimension (i.e. for each i,j residue pair)
-    sumArray = self.array.sum(dim="network")
+    def __init__ (self, args):
+        self.args = args
 
-    # Scales the sum array to all be values between 0 and 20
-    if self.args.no_scale_sum_network == False:
-        sumArray = self.scaleSumNetwork(sumArray)
-        logging.info(f'Scaled the Sum Network to values between 0 and 20')
+    def readPickle (self):
+        
+        # Opens pickle file
+        with open(self.args.filename, 'rb') as pickleFile:
+            self.array = pickle.load(pickleFile)
+            print(self.array)
 
-    logging.info(f'Created the sum matrix of all networks')
-    return sumArray
+    def calculateSum (self):
 
-def scaleSumNetwork (self, inputArray):
+        # Calculates the sum across the network dimension (i.e. for each i,j residue pair)
+        sumArray = self.array.sum(dim="network")
 
-    # Gets maximum value across all dimensions
-    maxValue = inputArray.max(dim=['firstResi','secondResi']).item()
+        # Scales the sum array to all be values between 0 and 20
+        if self.args.no_scale_sum_network == False:
+            sumArray = self.scaleSumNetwork(sumArray)
+            logging.info(f'Scaled the Sum Network to values between 0 and 20')
 
-    # Then divides each network by max value
-    # Scales to a set value (default 0 to 20)
-    scaledInputArray = (inputArray / maxValue) * self.args.sum_network_scale
+        logging.info(f'Created the sum matrix of all networks')
+        return sumArray
 
-    return scaledInputArray
+    def scaleSumNetwork (self, inputArray):
 
-def removeWeakEdges (self, inputArray):
-    
-    # Get maximum value across the entire array
-    # Then multiply that by the percent cutoff threshold specified by user
-    percentCutoffValue = inputArray.max() * (self.args.remove_weak_edges / 100)
+        # Gets maximum value across all dimensions
+        maxValue = inputArray.max(dim=['firstResi','secondResi']).item()
 
-    # Finds entries in the array where they are less than the percent cutoff threshold
-    # Then it replaces them with 0.0
-    # If not, then it keeps the original value
-    return xr.where(inputArray < percentCutoffValue, 0.0, inputArray)
+        # Then divides each network by max value
+        # Scales to a set value (default 0 to 20)
+        scaledInputArray = (inputArray / maxValue) * self.args.sum_network_scale
 
-def visualize (self, inputarray, outputname):
+        return scaledInputArray
 
-    # Removes weak edges so that the network is easier to visualize
-    if self.args.remove_weak_edges != None:
-        inputarray = self.removeWeakEdges(inputarray)
-        logging.info(f'Removed weak edges from array for visualization')
+    def removeWeakEdges (self, inputArray):
+        
+        # Get maximum value across the entire array
+        # Then multiply that by the percent cutoff threshold specified by user
+        percentCutoffValue = inputArray.max() * (self.args.remove_weak_edges / 100)
 
-    # Converts XArray into Numpy array
-    nparray = inputarray.to_numpy()
+        # Finds entries in the array where they are less than the percent cutoff threshold
+        # Then it replaces them with 0.0
+        # If not, then it keeps the original value
+        return xr.where(inputArray < percentCutoffValue, 0.0, inputArray)
 
-    # Creates graph from Numpy array
-    G = nx.from_numpy_array(nparray)
-    G.remove_nodes_from(list(nx.isolates(G)))
-    nx.convert_node_labels_to_integers(G)
-    
-    for i in G.nodes():
-        G.nodes[i]['label'] = str(i)
-    
-    # widths = nx.get_edge_attributes(G, 'weight')
+    def visualize (self, inputarray, outputname):
 
-    nts = Network(notebook=True)
-    
-    # populates the nodes and edges data structures
-    nts.from_nx(G)
-    outputpath = f'{self.args.output}{outputname}.html'
-    nts.show(outputpath)
+        # Removes weak edges so that the network is easier to visualize
+        if self.args.remove_weak_edges != None:
+            inputarray = self.removeWeakEdges(inputarray)
+            logging.info(f'Removed weak edges from array for visualization')
+
+        # Converts XArray into Numpy array
+        nparray = inputarray.to_numpy()
+
+        # Creates graph from Numpy array
+        G = nx.from_numpy_array(nparray)
+        G.remove_nodes_from(list(nx.isolates(G)))
+        nx.convert_node_labels_to_integers(G)
+        
+        for i in G.nodes():
+            G.nodes[i]['label'] = str(i)
+        
+        # widths = nx.get_edge_attributes(G, 'weight')
+
+        nts = Network(notebook=True)
+        
+        # populates the nodes and edges data structures
+        nts.from_nx(G)
+        outputpath = f'{self.args.output}{outputname}.html'
+        nts.show(outputpath)
