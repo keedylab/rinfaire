c608ed0d3c6e6187a2f35815259f6a08b393f256
Akshay Raju <akshay.raju@macaulay.cuny.edu>
2023-09-29 13:35:05 -0400

    Updated analysis_sum script
    - This is to better reflect the new changes in the SumNetwork class and how the functions are structured
    - Since the class functions all work on the self object there's no need to create intermediary variables here
    - Also added flags for the output file and whether or not the user wants the output to be a pickle file or a .html visualization

--- scripts/analysis_sum.py
+++ scripts/analysis_sum.py
@@ -15,6 +15,19 @@ def setupArguments ():
         help='Pickle (.pkl) file of the MultiNetwork object' 
     )
 
+    parser.add_argument(
+        'outputname', 
+        help='Name of the output file' 
+    )
+
+    parser.add_argument(
+        '-p',
+        '--output_pickle',
+        default=False,
+        action='store_true',
+        help='Flag to output pickle file instead of .html visualization' 
+    )
+
     parser.add_argument( 
         '--no_scale_sum_network', 
         default=False,
@@ -41,8 +54,22 @@ def setupArguments ():
 
 def main ():
     
+    # Sets up arguments inputted from user
     args = setupArguments()
+
+    # Creates SumNetwork object and imports user inputted pickle file 
     sumNetObject = SumNetwork(args)
+
+    # Calculates the sum network and performs scaling and removal of weak edges if flags are specified
+    sumNetObject.calculateSum()
+
+    # If the user wants to output a pickle file, then it calls on the outputPickle() function
+    # If not then it gets visualized through PyVis which creates a .html output
+    # Both files use the same file extension provided by the outputname argument
+    if args.output_pickle == True:
+        sumNetObject.outputPickle()
+    else:
+        sumNetObject.visualize()
 
 if __name__ == "__main__":
     main()
