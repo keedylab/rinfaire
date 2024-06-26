96c5b63f8237f987fd746543af14374aa7035fd1
Akshay Raju <akshay.raju@macaulay.cuny.edu>
2024-06-26 19:46:32 -0400

    Added Sum Network Scaling by Structures
    - Toggled by the --scale_sum_network flag
    - By default will scale by the number of structures

--- examples/run_analysis_sum_batch.sh
+++ examples/run_analysis_sum_batch.sh
@@ -1,14 +1,15 @@
 
 inputdir='/data/araju/ptpsinpdb/MultiRIN_Outputs/batchruns/run39/'
-outputfilename='/data/araju/ptpsinpdb/MultiRIN_Outputs/batchruns/run39/SumNetwork_Removed95_Mod7'
+outputfilename='/data/araju/ptpsinpdb/MultiRIN_Outputs/batchruns/run39/SumNetwork_Struct_Scaling'
 
 pipenv run python ../scripts/analysis_sum.py \
 	${inputdir}MultiNetwork.pkl \
 	${outputfilename} \
-	--remove_weak_edges 95 \
-	--seq_to_ref /data/araju/ptpsinpdb/FinalProcessing/Final/1SUG_qFit_chainA.pdb \
-	--remove_subgraphs 5 \
-	--detect_communities \
-	--output_modularity \
-	--n_communities 7 \
+	--remove_weak_edges 0 \
+	--scale_sum_network struct \
+	# --seq_to_ref /data/araju/ptpsinpdb/FinalProcessing/Final/1SUG_qFit_chainA.pdb \
+	# --remove_subgraphs 5 \
+	# --detect_communities \
+	# --output_modularity \
+	# --n_communities 7 \
 	# --output_pickle 
